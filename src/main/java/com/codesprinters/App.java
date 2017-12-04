4879181600ecb30ccdf801a37a2683245730a65c
OPMA <marcin.szkoleniowy@gmail.com>
2017-12-04 09:42:37 +0100

    Git integration test

---
+++ src/main/java/com/codesprinters/App.java
@@ -0,0 +1,13 @@
+package com.codesprinters;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
