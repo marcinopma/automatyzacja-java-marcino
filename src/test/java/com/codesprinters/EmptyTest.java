d7281e6c49e8d797fa904eb03eff68c2aaf7e9ee
OPMA <marcin.szkoleniowy@gmail.com>
2017-12-04 09:48:05 +0100

    Git integration test 2

---
+++ src/test/java/com/codesprinters/EmptyTest.java
@@ -0,0 +1,13 @@
+package com.codesprinters;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class EmptyTest {
+ @Test
+ public void shouldTestSomething() {
+  assertEquals("true", "false");
+ }
+}
